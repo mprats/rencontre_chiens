252cb7213808c7e3db08b964677216e898a1aad2
Maximilien <m.prats656@laposte.net>
2017-01-16 08:18:20 +0000

    Implemeting owners/dogs relations

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   devise_for :users, :controllers => { registrations: 'users/registrations' }
+  
+  
+  #resource :user do
+    #resources :dogs
+  #end
+  
+  resources :dogs
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -63,5 +70,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  resources :dogs
+  
 end
